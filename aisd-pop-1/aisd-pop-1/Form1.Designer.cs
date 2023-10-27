49ae78851f7f312592dc1b6e43b50bb1aa6118ba
Wojciech Kowalczyk <wwwojtus0@gmail.com>
2023-10-27 19:59:28 +0200

    2

--- aisd-pop-1/aisd-pop-1/Form1.Designer.cs
+++ aisd-pop-1/aisd-pop-1/Form1.Designer.cs
@@ -28,186 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnstart = new System.Windows.Forms.Button();
-            this.nudN = new System.Windows.Forms.NumericUpDown();
-            this.textNumbersM = new System.Windows.Forms.TextBox();
-            this.textNumbersSorted = new System.Windows.Forms.TextBox();
-            this.checkBox_auto = new System.Windows.Forms.CheckBox();
-            this.nmrcNumbersA = new System.Windows.Forms.NumericUpDown();
-            this.btnGen = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.labelTime = new System.Windows.Forms.Label();
-            this.buttonSB = new System.Windows.Forms.Button();
-            this.buttonSS = new System.Windows.Forms.Button();
-            this.buttonSI = new System.Windows.Forms.Button();
-            this.buttonSM = new System.Windows.Forms.Button();
-            this.buttonSQ = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.nudN)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nmrcNumbersA)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // btnstart
-            // 
-            this.btnstart.AccessibleName = "Default";
-            this.btnstart.Location = new System.Drawing.Point(413, 384);
-            this.btnstart.Name = "btnstart";
-            this.btnstart.Size = new System.Drawing.Size(112, 50);
-            this.btnstart.TabIndex = 0;
-            this.btnstart.Text = "start";
-            this.btnstart.UseVisualStyleBackColor = true;
-            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
-            // 
-            // nudN
-            // 
-            this.nudN.Location = new System.Drawing.Point(405, 355);
-            this.nudN.Name = "nudN";
-            this.nudN.Size = new System.Drawing.Size(120, 23);
-            this.nudN.TabIndex = 1;
+            textNumbersM = new TextBox();
+            textNumbersSorted = new TextBox();
+            checkBox_auto = new CheckBox();
+            nmrcNumbersA = new NumericUpDown();
+            btnGen = new Button();
+            label1 = new Label();
+            labelTime = new Label();
+            buttonSB = new Button();
+            buttonSS = new Button();
+            buttonSI = new Button();
+            buttonSM = new Button();
+            buttonSQ = new Button();
+            ((System.ComponentModel.ISupportInitialize)nmrcNumbersA).BeginInit();
+            SuspendLayout();
             // 
             // textNumbersM
             // 
-            this.textNumbersM.Location = new System.Drawing.Point(32, 30);
-            this.textNumbersM.Name = "textNumbersM";
-            this.textNumbersM.Size = new System.Drawing.Size(187, 23);
-            this.textNumbersM.TabIndex = 2;
-            this.textNumbersM.TextChanged += new System.EventHandler(this.textNumbersM_TextChanged);
+            textNumbersM.Location = new Point(37, 40);
+            textNumbersM.Margin = new Padding(3, 4, 3, 4);
+            textNumbersM.Name = "textNumbersM";
+            textNumbersM.Size = new Size(213, 27);
+            textNumbersM.TabIndex = 2;
+            textNumbersM.TextChanged += textNumbersM_TextChanged;
             // 
             // textNumbersSorted
             // 
-            this.textNumbersSorted.Location = new System.Drawing.Point(32, 172);
-            this.textNumbersSorted.Name = "textNumbersSorted";
-            this.textNumbersSorted.Size = new System.Drawing.Size(187, 23);
-            this.textNumbersSorted.TabIndex = 3;
-            this.textNumbersSorted.TextChanged += new System.EventHandler(this.textNumbersSorted_TextChanged);
+            textNumbersSorted.Location = new Point(37, 229);
+            textNumbersSorted.Margin = new Padding(3, 4, 3, 4);
+            textNumbersSorted.Name = "textNumbersSorted";
+            textNumbersSorted.Size = new Size(213, 27);
+            textNumbersSorted.TabIndex = 3;
+            textNumbersSorted.TextChanged += textNumbersSorted_TextChanged;
             // 
             // checkBox_auto
             // 
-            this.checkBox_auto.AutoSize = true;
-            this.checkBox_auto.Location = new System.Drawing.Point(222, 5);
-            this.checkBox_auto.Name = "checkBox_auto";
-            this.checkBox_auto.Size = new System.Drawing.Size(118, 19);
-            this.checkBox_auto.TabIndex = 4;
-            this.checkBox_auto.Tag = "";
-            this.checkBox_auto.Text = "Automat/manual";
-            this.checkBox_auto.UseVisualStyleBackColor = true;
-            this.checkBox_auto.CheckedChanged += new System.EventHandler(this.checkBox_auto_CheckedChanged);
+            checkBox_auto.AutoSize = true;
+            checkBox_auto.Location = new Point(254, 7);
+            checkBox_auto.Margin = new Padding(3, 4, 3, 4);
+            checkBox_auto.Name = "checkBox_auto";
+            checkBox_auto.Size = new Size(144, 24);
+            checkBox_auto.TabIndex = 4;
+            checkBox_auto.Tag = "";
+            checkBox_auto.Text = "Automat/manual";
+            checkBox_auto.UseVisualStyleBackColor = true;
+            checkBox_auto.CheckedChanged += checkBox_auto_CheckedChanged;
             // 
             // nmrcNumbersA
             // 
-            this.nmrcNumbersA.Location = new System.Drawing.Point(337, 31);
-            this.nmrcNumbersA.Name = "nmrcNumbersA";
-            this.nmrcNumbersA.Size = new System.Drawing.Size(120, 23);
-            this.nmrcNumbersA.TabIndex = 5;
-            this.nmrcNumbersA.ValueChanged += new System.EventHandler(this.nmrcNumbersA_ValueChanged);
+            nmrcNumbersA.Location = new Point(385, 41);
+            nmrcNumbersA.Margin = new Padding(3, 4, 3, 4);
+            nmrcNumbersA.Name = "nmrcNumbersA";
+            nmrcNumbersA.Size = new Size(137, 27);
+            nmrcNumbersA.TabIndex = 5;
+            nmrcNumbersA.ValueChanged += nmrcNumbersA_ValueChanged;
             // 
             // btnGen
             // 
-            this.btnGen.Location = new System.Drawing.Point(337, 94);
-            this.btnGen.Name = "btnGen";
-            this.btnGen.Size = new System.Drawing.Size(75, 23);
-            this.btnGen.TabIndex = 6;
-            this.btnGen.Text = "generuj";
-            this.btnGen.UseVisualStyleBackColor = true;
-            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            btnGen.Location = new Point(385, 125);
+            btnGen.Margin = new Padding(3, 4, 3, 4);
+            btnGen.Name = "btnGen";
+            btnGen.Size = new Size(86, 31);
+            btnGen.TabIndex = 6;
+            btnGen.Text = "generuj";
+            btnGen.UseVisualStyleBackColor = true;
+            btnGen.Click += btnGen_Click;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(307, 172);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 15);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Czas";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            label1.AutoSize = true;
+            label1.Location = new Point(351, 229);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Czas";
+            label1.Click += label1_Click;
             // 
             // labelTime
             // 
-            this.labelTime.AutoSize = true;
-            this.labelTime.Location = new System.Drawing.Point(374, 172);
-            this.labelTime.Name = "labelTime";
-            this.labelTime.Size = new System.Drawing.Size(38, 15);
-            this.labelTime.TabIndex = 8;
-            this.labelTime.Text = "label2";
+            labelTime.AutoSize = true;
+            labelTime.Location = new Point(427, 229);
+            labelTime.Name = "labelTime";
+            labelTime.Size = new Size(17, 20);
+            labelTime.TabIndex = 8;
+            labelTime.Text = "0";
+            labelTime.Click += labelTime_Click;
             // 
             // buttonSB
             // 
-            this.buttonSB.Location = new System.Drawing.Point(32, 289);
-            this.buttonSB.Name = "buttonSB";
-            this.buttonSB.Size = new System.Drawing.Size(75, 23);
-            this.buttonSB.TabIndex = 9;
-            this.buttonSB.Text = "SB";
-            this.buttonSB.UseVisualStyleBackColor = true;
-            this.buttonSB.Click += new System.EventHandler(this.buttonSB_Click);
+            buttonSB.Location = new Point(37, 385);
+            buttonSB.Margin = new Padding(3, 4, 3, 4);
+            buttonSB.Name = "buttonSB";
+            buttonSB.Size = new Size(86, 31);
+            buttonSB.TabIndex = 9;
+            buttonSB.Text = "SB";
+            buttonSB.UseVisualStyleBackColor = true;
+            buttonSB.Click += buttonSB_Click;
             // 
             // buttonSS
             // 
-            this.buttonSS.Location = new System.Drawing.Point(127, 289);
-            this.buttonSS.Name = "buttonSS";
-            this.buttonSS.Size = new System.Drawing.Size(75, 23);
-            this.buttonSS.TabIndex = 10;
-            this.buttonSS.Text = "SS";
-            this.buttonSS.UseVisualStyleBackColor = true;
+            buttonSS.Location = new Point(145, 385);
+            buttonSS.Margin = new Padding(3, 4, 3, 4);
+            buttonSS.Name = "buttonSS";
+            buttonSS.Size = new Size(86, 31);
+            buttonSS.TabIndex = 10;
+            buttonSS.Text = "SS";
+            buttonSS.UseVisualStyleBackColor = true;
+            buttonSS.Click += buttonSS_Click;
             // 
             // buttonSI
             // 
-            this.buttonSI.Location = new System.Drawing.Point(222, 289);
-            this.buttonSI.Name = "buttonSI";
-            this.buttonSI.Size = new System.Drawing.Size(75, 23);
-            this.buttonSI.TabIndex = 11;
-            this.buttonSI.Text = "SI";
-            this.buttonSI.UseVisualStyleBackColor = true;
+            buttonSI.Location = new Point(254, 385);
+            buttonSI.Margin = new Padding(3, 4, 3, 4);
+            buttonSI.Name = "buttonSI";
+            buttonSI.Size = new Size(86, 31);
+            buttonSI.TabIndex = 11;
+            buttonSI.Text = "SI";
+            buttonSI.UseVisualStyleBackColor = true;
             // 
             // buttonSM
             // 
-            this.buttonSM.Location = new System.Drawing.Point(320, 289);
-            this.buttonSM.Name = "buttonSM";
-            this.buttonSM.Size = new System.Drawing.Size(75, 23);
-            this.buttonSM.TabIndex = 12;
-            this.buttonSM.Text = "SM";
-            this.buttonSM.UseVisualStyleBackColor = true;
+            buttonSM.Location = new Point(366, 385);
+            buttonSM.Margin = new Padding(3, 4, 3, 4);
+            buttonSM.Name = "buttonSM";
+            buttonSM.Size = new Size(86, 31);
+            buttonSM.TabIndex = 12;
+            buttonSM.Text = "SM";
+            buttonSM.UseVisualStyleBackColor = true;
             // 
             // buttonSQ
             // 
-            this.buttonSQ.Location = new System.Drawing.Point(422, 289);
-            this.buttonSQ.Name = "buttonSQ";
-            this.buttonSQ.Size = new System.Drawing.Size(75, 23);
-            this.buttonSQ.TabIndex = 13;
-            this.buttonSQ.Text = "SQ";
-            this.buttonSQ.UseVisualStyleBackColor = true;
+            buttonSQ.Location = new Point(482, 385);
+            buttonSQ.Margin = new Padding(3, 4, 3, 4);
+            buttonSQ.Name = "buttonSQ";
+            buttonSQ.Size = new Size(86, 31);
+            buttonSQ.TabIndex = 13;
+            buttonSQ.Text = "SQ";
+            buttonSQ.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.buttonSQ);
-            this.Controls.Add(this.buttonSM);
-            this.Controls.Add(this.buttonSI);
-            this.Controls.Add(this.buttonSS);
-            this.Controls.Add(this.buttonSB);
-            this.Controls.Add(this.labelTime);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnGen);
-            this.Controls.Add(this.nmrcNumbersA);
-            this.Controls.Add(this.checkBox_auto);
-            this.Controls.Add(this.textNumbersSorted);
-            this.Controls.Add(this.textNumbersM);
-            this.Controls.Add(this.nudN);
-            this.Controls.Add(this.btnstart);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.nudN)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nmrcNumbersA)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(914, 600);
+            Controls.Add(buttonSQ);
+            Controls.Add(buttonSM);
+            Controls.Add(buttonSI);
+            Controls.Add(buttonSS);
+            Controls.Add(buttonSB);
+            Controls.Add(labelTime);
+            Controls.Add(label1);
+            Controls.Add(btnGen);
+            Controls.Add(nmrcNumbersA);
+            Controls.Add(checkBox_auto);
+            Controls.Add(textNumbersSorted);
+            Controls.Add(textNumbersM);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)nmrcNumbersA).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
-        private Button btnstart;
-        private NumericUpDown nudN;
         private TextBox textNumbersM;
         private TextBox textNumbersSorted;
         private CheckBox checkBox_auto;
--- aisd-pop-1/aisd-pop-1/Form1.cs
+++ aisd-pop-1/aisd-pop-1/Form1.cs
@@ -3,69 +3,20 @@ namespace aisd_pop_1
     public partial class Form1 : Form
     {
         bool manual = false;
+        int tyleLiczb;
+        string liczby;
+        int[] tablicaLiczb;
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void btnstart_Click(object sender, EventArgs e)
-        {
-            int n = (int)nudN.Value;
-            int wynik = Fib2(n);
-            string napis = wynik.ToString();
-            MessageBox.Show(napis);
-        }
-
-        int Fib(int n)
-        {
-            if (n == 0)
-                return 0;
-            if (n == 1)
-                return 1;
-            return Fib(n - 1) + Fib(n - 2);
-        }
-
-        int Fib2(int n)
-        {
-            int[] wyrazy = new int[n + 2];
-            wyrazy[0] = 0;
-            wyrazy[1] = 1;
-            for (int i = 2; i < n + 1; i++)
-            {
-                wyrazy[i] = wyrazy[i - 1] + wyrazy[i - 2];
-            }
-            return wyrazy[n];
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textNumbersM_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void buttonSB_Click(object sender, EventArgs e)
-        {
-            int wynik = 0;
-            textNumbersSorted.Text = wynik.ToString();
-        }
-
-        private void textNumbersSorted_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void checkBox_auto_CheckedChanged(object sender, EventArgs e)
         {
-            if(checkBox_auto.Checked.Equals(true))
+            if (checkBox_auto.Checked.Equals(true))
             {
                 textNumbersM.Enabled = true;
                 nmrcNumbersA.Enabled = false;
@@ -85,22 +36,89 @@ namespace aisd_pop_1
         {
             Random rnd = new Random();
             int tyleLiczb = (int)nmrcNumbersA.Value;
-            int[] tablicaLiczb = new int[tyleLiczb];
+            tablicaLiczb = new int[tyleLiczb];
             for (int i = 0; i < tyleLiczb; i++)
             {
                 tablicaLiczb[i] = rnd.Next(1000);
             }
-            string liczby = "";
-            for (int i = 0; i < liczby.Length; i++)
+            liczby = "";
+            for (int i = 0; i < tyleLiczb; i++)
             {
-                liczby.Append
+                liczby += tablicaLiczb[i].ToString() + " ";
             }
-            textNumbersM.Text = tablicaLiczb.ToString();
+            textNumbersM.Text = liczby;
         }
+        private void labelTime_Click(object sender, EventArgs e)
+        {
 
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void textNumbersM_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textNumbersSorted_TextChanged(object sender, EventArgs e)
+        {
+
+        }
         private void nmrcNumbersA_ValueChanged(object sender, EventArgs e)
         {
-            
+
+        }
+
+        private string tablicaNaString(int[] tablica)
+        {
+            string zwroc = "";
+            for (int i = 0; i < tyleLiczb; i++)
+            {
+                zwroc += tablica[i].ToString() + " ";
+            }
+            textNumbersM.Text = zwroc;
+            return zwroc;
+        }
+        private void buttonSB_Click(object sender, EventArgs e)
+        {
+            int wynik = 0;
+            bool another_one = true;
+            int liczba_temp;
+            textNumbersSorted.Text = wynik.ToString();
+            while (another_one)
+            {
+                another_one = false;
+                for (int i = 1; i < tyleLiczb; i++)
+                {
+                    if (tablicaLiczb[i] > tablicaLiczb[i - 1])
+                    {
+                        liczba_temp = tablicaLiczb[i];
+                        tablicaLiczb[i] = tablicaLiczb[i - 1];
+                        tablicaLiczb[i - 1] = liczba_temp;
+                        another_one = true;
+                    }
+
+                }
+            }
+            MessageBox.Show(tablicaLiczb[0].ToString());
+            textNumbersSorted.Text = tablicaNaString(tablicaLiczb);
+
+        }
+        private void buttonSS_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonSI_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonSM_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonSQ_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
